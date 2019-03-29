664777ab2f2b4094aa21f0dba68c3de92464eeb6
Chris Kalahiki <chris.kalahiki@gmail.com>
2019-03-29 08:25:25 -0500

    Better optimized caesar-cipher.py

--- hw1-kalahiki/caesar-cipher.py
+++ hw1-kalahiki/caesar-cipher.py
@@ -1,33 +1,39 @@
 # Et tu, Brute?
-#
-# Chris Kalahiki
+# by: Chris Kalahiki
 import sys
 
+# Hard-coded alphabet
 ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\|;:'\",<.>/? "
 
+# Importing dictionary and stdin to lists
 DICT = open("dictionary.txt", "r")
 DICT = DICT.read()
-DICT = [i for i in DICT.split('\n')]
+DICT = [i.lower() for i in DICT.split('\n')]
+INPUT = sys.stdin.readlines()
+INPUT = [i.strip('\n') for i in INPUT]
 
+# Encrypt function
 def encrypt(plaintext, shift):
     shifted_alphabet = ALPHABET[shift:] + ALPHABET[:shift]
     table = str.maketrans(ALPHABET, shifted_alphabet)
     return plaintext.translate(table)
 
+# Decrypt function
 def decrypt(ciphertext, shift):
     shifted_alphabet = ALPHABET[shift:] + ALPHABET[:shift]
     table = str.maketrans(shifted_alphabet, ALPHABET)
     return [i.translate(table) for i in ciphertext]
 
+# Check for plaintext
 def isPlaintext(plaintext):
-    match = 0
-    for i in DICT:
-        for k in plaintext:
-            for j in k.split():
-                if i == j: match += 1
-    return True if match > 3 else False # This failed in at least 1 test case 
+    match, count = 0, 0
+    for i in plaintext:
+        for j in i.split():
+            if j.lower().strip("`~!@#$%^&*()-_=+[{]}\|;:\",<.>/?") in DICT: match += 1
+            count += 1
+    perc = float(match)/float(count)
+    return True if perc >= 0.9 else False # This failed in at least 1 test case 
 
-INPUT = sys.stdin.readlines()
 for i in range(95):
     if isPlaintext(decrypt(INPUT, i)) == True:
         print("SHIFT=", i)
