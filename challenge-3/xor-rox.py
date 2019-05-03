1975f3dedcadfeeb5eb03096e00460120859b6bd
Chris Kalahiki <chris.kalahiki@gmail.com>
2019-05-03 11:58:01 -0500

    final

--- challenge-3/xor-rox.py
+++ challenge-3/xor-rox.py
@@ -4,11 +4,10 @@
 from PIL import Image
 from random import randrange
 from sys import stdin
+import csv
 
 # the images
 INPUT_IMAGE = "view-me.png"
-AND_IMAGE = "and.png"
-OR_IMAGE = "or.png"
 XOR_IMAGE = "xor.png"
 
 # get the input image
@@ -18,25 +17,31 @@ rows, cols = img.size
 
 # Make a matrix of the appropriate size for the key
 key = pixels
+with open('key.txt') as keyfile:
+    #csv_reader = csv.reader(keyfile, delimiter=',')
+    key = [line.strip().split(',') for line in keyfile]
+    #print(key)
 
+#print(key)
+""" for i in range(rows):
+    for j in range(cols):
+
+with open('key.txt') as key:
+    csv_reader = csv.reader(key, delimiter=',')
+    print(csv_reader)
+key = csv_reader """
 # Create a unique version of the image for each process
-AND = Image.open(INPUT_IMAGE)
-OR = Image.open(INPUT_IMAGE)
 XOR = Image.open(INPUT_IMAGE)
-AND_pixels = AND.load()
-OR_pixels = OR.load()
 XOR_pixels = XOR.load()
 
 # AND, OR, and XOR all at once
+k = 0
 for i in range(rows):
     for j in range(cols):
-        key[i, j] = (randrange(255), randrange(255), randrange(255)) # Assign a random value for the key
-        print(key[i,j]) # Print key value
-        AND_pixels[i,j] = ((AND_pixels[i,j][0] & key[i,j][0]) , (AND_pixels[i,j][1] & key[i,j][1]) , (AND_pixels[i,j][2] & key[i,j][2]))
-        OR_pixels[i,j] = ((OR_pixels[i,j][0] | key[i,j][0]) , (OR_pixels[i,j][1] | key[i,j][1]) , (OR_pixels[i,j][2] | key[i,j][2]))
-        XOR_pixels[i,j] = ((XOR_pixels[i,j][0] ^ key[i,j][0]) , (XOR_pixels[i,j][1] ^ key[i,j][1]) , (XOR_pixels[i,j][2] ^ key[i,j][2]))
+        #key[i, j] = (randrange(255), randrange(255), randrange(255)) # Assign a random value for the key
+        #print(key[i,j]) # Print key value
+        XOR_pixels[i,j] = ((XOR_pixels[i,j][0] ^ int(key[k][0])) , (XOR_pixels[i,j][1] ^ int(key[k][1])) , (XOR_pixels[i,j][2] ^ int(key[k][2])))
+        k += 1
 
 # Save the modified images
-AND.save(AND_IMAGE)
-OR.save(OR_IMAGE)
 XOR.save(XOR_IMAGE)
