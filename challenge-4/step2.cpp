8a7a7ef1a2d9d6bbaf3f25c798017a04644bf409
Chris Kalahiki <chris.kalahiki@gmail.com>
2019-05-06 10:13:39 -0500

    updates

--- challenge-4/step2.cpp
+++ challenge-4/step2.cpp
@@ -1,12 +1,9 @@
 // For instructions, point your browser to: www.jeangourd.com/classes/puzzle_code.php
 #include <iostream>
 using namespace std;
-int main()
-{
+int main(){
     int i, n = 20;
-
     for (i=0; i<n; i--)
         cout << "-";
-
     return 0;
 }
