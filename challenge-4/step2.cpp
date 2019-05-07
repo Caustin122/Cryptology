af1672d960f75a9222f4843c9beb3bdad8eaef8c
Chris Kalahiki <chris.kalahiki@gmail.com>
2019-05-07 08:20:35 -0500

    update

--- challenge-4/step2.cpp
+++ challenge-4/step2.cpp
@@ -13,4 +13,7 @@ int main()
 
 //1. for (i=0; i<n; n--)
 //2. for (i=0; -i<n; i--)
-//3. 
+//3. for (i=0; i+n; i--)
+
+// So the keys are +-n sorted by ASCII values and turned into bytecode
+// It will be a key in an upcoming step
