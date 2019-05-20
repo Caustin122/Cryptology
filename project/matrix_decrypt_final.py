fa019b6824c8a892c70c67ae559ddb9eee0846a0
Chris Kalahiki <chris.kalahiki@gmail.com>
2019-05-19 21:30:12 -0500

    Working Decrypt function

--- project/matrix_decrypt_final.py
+++ project/matrix_decrypt_final.py
@@ -34,22 +34,22 @@ def shift_calc(time):
     second =  int(time[2])
     shift = []
 
-    # Hour Shift
-    if hour == 1 or hour == 2:
+    # Second Shift
+    if second >= 5 and second <= 10:
         shift.append(2)
-    elif hour == 3:
+    elif second >= 11 and second <= 19:
         shift.append(5)
-    elif hour == 4 or hour == 5:
+    elif second >= 20 and second <= 25:
         shift.append(8)
-    elif hour == 6:
+    elif second >= 26 and second <= 34:
         shift.append(7)
-    elif hour == 7 or hour == 8:
+    elif second >= 35 and second <= 40:
         shift.append(6)
-    elif hour == 9:
+    elif second >= 41 and second <= 49:
         shift.append(3)
-    elif hour == 10 or hour == 11:
+    elif second >= 50 and second <= 55:
         shift.append(0)
-    elif hour == 12:
+    elif (second >= 56 and second <= 59) or (second >= 0 and second <= 4):
         shift.append(1)
     else:
         return -1
@@ -74,22 +74,22 @@ def shift_calc(time):
     else:
         return -2
 
-    # Second Shift
-    if second >= 5 and second <= 10:
+    # Hour Shift
+    if hour == 1 or hour == 2:
         shift.append(2)
-    elif second >= 11 and second <= 19:
+    elif hour == 3:
         shift.append(5)
-    elif second >= 20 and second <= 25:
+    elif hour == 4 or hour == 5:
         shift.append(8)
-    elif second >= 26 and second <= 34:
+    elif hour == 6:
         shift.append(7)
-    elif second >= 35 and second <= 40:
+    elif hour == 7 or hour == 8:
         shift.append(6)
-    elif second >= 41 and second <= 49:
+    elif hour == 9:
         shift.append(3)
-    elif second >= 50 and second <= 55:
+    elif hour == 10 or hour == 11:
         shift.append(0)
-    elif (second >= 56 and second <= 59) or (second >= 0 and second <= 4):
+    elif hour == 12:
         shift.append(1)
     else:
         return -3
